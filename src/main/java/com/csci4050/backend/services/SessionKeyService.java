c334415de78755873a0c9ad003a616ca7763c2b7
George Herrmann <gvhmann@gmail.com>
2023-03-15 11:57:19 -0400

    SessionKeyService written

--- src/main/java/com/csci4050/backend/services/SessionKeyService.java
+++ src/main/java/com/csci4050/backend/services/SessionKeyService.java
@@ -1,5 +1,18 @@
 package com.csci4050.backend.services;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+
 /**
  * Create a class which features a basic service which can create and verify session keys.
  * We are going to use JWT tokens for session verification. I have already imported it.
@@ -8,5 +21,78 @@ package com.csci4050.backend.services;
  * Implementator: George
  */
 public class SessionKeyService {
-    
+
+    private RSAPublicKey publicKey;
+    private RSAPrivateKey privateKey;
+
+    /**
+     * Creates a SessionKeyService, generating a new public and private key pair.
+     * All session keys to be verified by this service must have been created by this service.
+     */
+    public SessionKeyService() {
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(2048);
+            KeyPair kp = kpg.generateKeyPair();
+
+            publicKey = (RSAPublicKey) kp.getPublic();
+            privateKey = (RSAPrivateKey) kp.getPrivate();
+        } catch (NoSuchAlgorithmException e) {
+            throw new JWTCreationException("Error generating key pairs, invalid configuration", e);
+        }
+    }
+
+    /**
+     * Creates a new session key for this service.
+     * 
+     * @return A new session key.
+     * @throws JWTCreationException If the key pair could not be created.
+     */
+    public String createSessionKey() throws JWTCreationException {
+        try {
+            Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
+            return  JWT.create().withIssuer("auth0").sign(algorithm);
+        } catch (JWTCreationException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    /**
+     * Verifies a session key for this service.
+     * 
+     * @param token The session key to verify.
+     * @return True if the session key is valid, false otherwise.
+     */
+    public boolean verifySessionKey(String token) {
+        try {
+            Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
+            JWTVerifier verifier = JWT.require(algorithm).withIssuer("auth0").build();
+            verifier.verify(token);
+            return true;
+        } catch (JWTVerificationException exception) {
+            exception.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Verifies a session key for this service and returns the decoded JWT if
+     * successful.
+     * 
+     * @param token The session key to verify.
+     * @return The decoded JWT if the session key is valid, null otherwise.
+     * @throws JWTVerificationException If the session key is invalid.
+     */
+    public DecodedJWT verifyAndGetJWT(String token) throws JWTVerificationException {
+        try {
+            Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
+            JWTVerifier verifier = JWT.require(algorithm).withIssuer("auth0").build();
+                
+            return verifier.verify(token);
+        } catch (JWTVerificationException exception) {
+            exception.printStackTrace();
+            throw exception;
+        }
+    }
 }
